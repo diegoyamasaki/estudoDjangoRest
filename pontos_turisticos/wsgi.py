ace2bc419a67b16ea62ed984d26c45970f9b4c6b
Diego Yamasaki <yamasaki.diego@gmail.com>
2022-11-02 17:25:53 -0300

    ajuste

--- pontos_turisticos/wsgi.py
+++ pontos_turisticos/wsgi.py
@@ -13,5 +13,5 @@ from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pontos_turisticos.settings')
 
-# application = Cling(get_wsgi_application())
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+# application = get_wsgi_application()
