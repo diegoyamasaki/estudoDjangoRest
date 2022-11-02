9c5b24eb007564f459926ef74032830c6b29a008
Diego Yamasaki <yamasaki.diego@gmail.com>
2022-11-02 17:36:44 -0300

    ajuste

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = [
-    '*'
+    'pontos-turisticos-diego.herokuapp.com',
+    'localhost',
+    '127.0.0.1'
 ]
 
 
